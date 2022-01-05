0830b0b706696c374428ad127c6087a1b2d69533
WillWatkins <w.watkins@hotmail.co.uk>
2022-01-05 16:15:56 +0000

    Refactor for styling

--- components/EditProfile.screen/EditProfile.component.tsx
+++ components/EditProfile.screen/EditProfile.component.tsx
@@ -2,24 +2,21 @@ import React, { useState, useEffect, useContext } from "react";
 import {
   Text,
   TextInput,
-  Button,
   Image,
   Platform,
   View,
   Alert,
-  KeyboardAvoidingView,
-  TouchableWithoutFeedback,
-  Keyboard,
   TouchableOpacity,
+  Dimensions,
 } from "react-native";
 import { styles } from "./EditProfile.style";
 import { UserContext } from "../../contexts/UserContext";
-import { SafeAreaView } from "react-native-safe-area-context";
 import { updateUserDetails, deleteUser } from "../../utils/firebaseUtils";
+import { ScrollView } from "react-native-gesture-handler";
+import { useKeyboard } from "@react-native-community/hooks";
 import * as ImagePicker from "expo-image-picker";
 import { storage } from "../../utils/firestoreConfig";
 import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
-import { ScrollView } from "react-native-gesture-handler";
 
 type UpdateUserProps = {
   navigation: {
@@ -31,6 +28,8 @@ export const EditProfile = ({ navigation }: UpdateUserProps) => {
   const { currentUser } = useContext(UserContext);
   const [imgURL, setImgURL] = useState("");
   const [userDetails, setUserDetails] = useState(currentUser);
+  const windowHeight = Dimensions.get("window").height;
+  const keyboard = useKeyboard();
 
   useEffect(() => {
     (async () => {
@@ -53,22 +52,10 @@ export const EditProfile = ({ navigation }: UpdateUserProps) => {
     navigation.navigate("Profile");
   };
 
-  const selectPhoto = async () => {
-    let result = await ImagePicker.launchImageLibraryAsync({
-      mediaTypes: ImagePicker.MediaTypeOptions.All,
-      allowsEditing: true,
-      aspect: [4, 3],
-      quality: 1,
-    });
-    if (!result.cancelled) {
-      uploadImage(result.uri, `avatar/img/${currentUser.id}`)
-        .then((res) => {
-          Alert.alert("Upload Successful");
-        })
-        .catch((err) => {
-          Alert.alert(err);
-        });
-    }
+  const deleteUserDetails = () => {
+    deleteUser(currentUser.id)
+      .then(() => {})
+      .catch(() => {});
   };
 
   const uploadImage = async (uri: string, imageName: string) => {
@@ -102,57 +89,78 @@ export const EditProfile = ({ navigation }: UpdateUserProps) => {
       });
   };
 
-  const deleteUserDetails = () => {
-    deleteUser(currentUser.id)
-      .then(() => {})
-      .catch(() => {});
+  const selectPhoto = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+    if (!result.cancelled) {
+      uploadImage(result.uri, `avatar/img/${currentUser.id}`)
+        .then((res) => {
+          Alert.alert("Upload Successful");
+        })
+        .catch((err) => {
+          Alert.alert(err);
+        });
+    }
   };
 
+  const [size, setSize] = useState(windowHeight);
+  useEffect(() => {
+    keyboard.keyboardShown
+      ? setSize(windowHeight * 0.82 - keyboard.keyboardHeight)
+      : setSize(windowHeight * 0.82);
+    setSize;
+  }, [keyboard.keyboardShown]);
+
   return (
-    <SafeAreaView style={styles.container}>
-      <ScrollView>
-        <Text style={styles.title}>Update Details</Text>
-        {imgURL ? (
-          <Image source={{ uri: imgURL }} style={styles.avatar} />
-        ) : null}
-        <Button onPress={selectPhoto} color="black" title="Choose Photo" />
-        <KeyboardAvoidingView
-          behavior={Platform.OS === "ios" ? "padding" : "height"}
-        >
-          <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-            <View style={styles.inputContainer}>
-              <Text style={styles.inputTitle}>First Name:</Text>
-              <TextInput
-                style={styles.inputField}
-                onChangeText={(text) => handleChange(text, "first_name")}
-                placeholder="First Name"
+    <View>
+      <Text style={styles.title}>UPDATE DETAILS</Text>
+      <View style={{ height: size }}>
+        <ScrollView>
+          <View style={styles.inputContainer}>
+            {currentUser.image_bitmap ? (
+              <Image
+                source={{ uri: currentUser.image_bitmap }}
+                style={styles.avatar}
               />
-              <Text style={styles.inputTitle}>Last Name:</Text>
-              <TextInput
-                style={styles.inputField}
-                onChangeText={(text) => handleChange(text, "last_name")}
-                placeholder="Last Name"
-              />
-              <Text style={styles.inputTitle}>Description:</Text>
-              <TextInput
-                style={styles.inputField}
-                onChangeText={(text) => handleChange(text, "description")}
-                placeholder="Description"
-              />
-            </View>
-          </TouchableWithoutFeedback>
-        </KeyboardAvoidingView>
-        <View style={styles.submit}>
-          <TouchableOpacity style={styles.submitButton} onPress={updateUser}>
-            <Text style={styles.submitText}>SUBMIT</Text>
+            ) : null}
+            <TouchableOpacity style={styles.submitButton} onPress={selectPhoto}>
+              <Text style={styles.submitText}>Update Photo</Text>
+            </TouchableOpacity>
+
+            <Text style={styles.inputTitle}>First Name:</Text>
+            <TextInput
+              style={styles.inputField}
+              onChangeText={(text) => handleChange(text, "first_name")}
+              placeholder="First Name"
+            />
+            <Text style={styles.inputTitle}>Last Name:</Text>
+            <TextInput
+              style={styles.inputField}
+              onChangeText={(text) => handleChange(text, "last_name")}
+              placeholder="Last Name"
+            />
+            <Text style={styles.inputTitle}>Description:</Text>
+            <TextInput
+              style={styles.inputField}
+              onChangeText={(text) => handleChange(text, "description")}
+              placeholder="Description"
+            />
+            <TouchableOpacity style={styles.submitButton} onPress={updateUser}>
+              <Text style={styles.submitText}>SUBMIT</Text>
+            </TouchableOpacity>
+          </View>
+          <TouchableOpacity
+            style={styles.deleteUserButton}
+            onPress={deleteUserDetails}
+          >
+            <Text style={styles.deleteButtonText}>Delete account?</Text>
           </TouchableOpacity>
-        </View>
-        <Button
-          onPress={deleteUserDetails}
-          color="red"
-          title="Delete Account"
-        />
-      </ScrollView>
-    </SafeAreaView>
+        </ScrollView>
+      </View>
+    </View>
   );
 };
