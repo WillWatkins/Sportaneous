69199c2354cdadaf7f5470aa6d1c07db102b7b8a
WillWatkins <w.watkins@hotmail.co.uk>
2022-01-05 13:08:37 +0000

    Removed console.log

--- components/Events.screen/RefreshEvents.component.tsx
+++ components/Events.screen/RefreshEvents.component.tsx
@@ -1,24 +1,21 @@
 import React from "react";
-import {Pressable, View, Text} from "react-native";
-import {selectAllEvents} from "../../utils/utils";
-import {styles} from "./Filter.style";
-import {resetSelection} from "./utils/FilterUtils";
+import { Pressable, View, Text } from "react-native";
+import { selectAllEvents } from "../../utils/utils";
+import { styles } from "./Filter.style";
+import { resetSelection } from "./utils/FilterUtils";
 
-export const RefreshEvents = ({setEvents, setCategoryIsChecked}) => {
+export const RefreshEvents = ({ setEvents, setCategoryIsChecked }) => {
   return (
     <View>
       <Pressable
-        style={({pressed}) => [
+        style={({ pressed }) => [
           {
-            backgroundColor: pressed
-              ? "silver"
-              : "grey",
+            backgroundColor: pressed ? "silver" : "grey",
           },
           styles.refreshButton,
         ]}
         onPress={() => {
           selectAllEvents().then((res) => {
-            console.log(setCategoryIsChecked);
             resetSelection(setCategoryIsChecked);
             setEvents(res);
           });
